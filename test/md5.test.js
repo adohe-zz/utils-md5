ffb16adfc56864dd522df286e9f0ae1134171647
AdoHe <coolhzb@163.com>
2014-03-13 11:27:06 +0800

    enable ci and add test file...

--- test/md5.test.js
+++ test/md5.test.js
@@ -1,1 +1,19 @@
-describe();
+"use strict";
+
+var md5 = require('../');
+var should = require('chai').should();
+
+describe('md5.test.js', function() {
+	describe('md5()', function() {
+		it('should be empty when the string is empty', function() {
+			var m = md5('');
+			m.should.not.be.empty;
+		});
+
+		it('should throw an error when the data is not a string or buffer', function() {
+			md5(null).should.throw();
+			md5({}).should.throw();
+			md5([]).should.throw();
+		});
+	});	
+});
